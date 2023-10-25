a3c5cd6e4265bc497ac21caf2c62c0e489b949dc
Ariful islam <arifulislam10111999@gmail.com>
2023-10-25 12:46:39 +0600

    new

--- src/middleware.js
+++ src/middleware.js
@@ -1,40 +1,40 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// // This function can be marked `async` if using `await` inside
-// export function middleware(request) {
-//   const path = request.nextUrl.pathname;
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+  const path = request.nextUrl.pathname;
 
-//   const isPublicPath = path === "/login" || path === "/signup";
-//   const token = request.cookies.get("token")?.value || "";
-//   console.log(request.cookies);
-//   if (isPublicPath && token) {
-//     return NextResponse.redirect(new URL("/", request.nextUrl));
-//   }
+  const isPublicPath = path === "/login" || path === "/signup";
+  // c = request.cookies.get("token")?.value || "";
+  // console.log(request.cookies);
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
 
-//   if (!isPublicPath && !token) {
-//     return NextResponse.redirect(new URL("/login", request.nextUrl));
-//   }
-//   // if (path != "/login") {
-//   //   return NextResponse.redirect(new URL("/login", request.nextUrl));
-//   // }
-//   // Set a flag in the request object
-//   request.showProfilePopup = !isPublicPath && !!token;
+  if (!isPublicPath) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+  // if (path != "/login") {
+  //   return NextResponse.redirect(new URL("/login", request.nextUrl));
+  // }
+  // Set a flag in the request object
+  request.showProfilePopup = !isPublicPath;
 
-//   if (!isPublicPath && !token) {
-//     return NextResponse.redirect(new URL("/login", request.nextUrl));
-//   }
-//   console.log(token, isPublicPath, path);
-// }
+  if (!isPublicPath) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+  console.log(path, isPublicPath);
+}
 
-// // See "Matching Paths" below to learn more
-// export const config = {
-//   matcher: [
-//     "/",
-//     "/login",
-//     "/signup",
-//     "/article",
-//     "/articleCreate",
-//     "/profilePopUp",
-//     "/profile",
-//   ],
-// };
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    "/",
+    "/login",
+    "/signup",
+    "/article",
+    "/articleCreate",
+    "/profilePopUp",
+    "/profile",
+  ],
+};
