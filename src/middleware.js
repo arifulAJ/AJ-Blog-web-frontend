e2aebfdaa334cbb8a8f739281d711c3850b7f0ec
Ariful islam <arifulislam10111999@gmail.com>
2023-10-25 12:51:06 +0600

    git

--- src/middleware.js
+++ src/middleware.js
@@ -5,25 +5,25 @@ export function middleware(request) {
   const path = request.nextUrl.pathname;
 
   const isPublicPath = path === "/login" || path === "/signup";
-  // c = request.cookies.get("token")?.value || "";
-  // console.log(request.cookies);
+  const token = request.cookies.get("token")?.value || "";
+  console.log(request.cookies);
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 
-  if (!isPublicPath) {
+  if (!isPublicPath && !token) {
     return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
   // if (path != "/login") {
   //   return NextResponse.redirect(new URL("/login", request.nextUrl));
   // }
   // Set a flag in the request object
-  request.showProfilePopup = !isPublicPath;
+  request.showProfilePopup = !isPublicPath && !!token;
 
-  if (!isPublicPath) {
+  if (!isPublicPath && !token) {
     return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
-  console.log(path, isPublicPath);
+  console.log(token, isPublicPath, path);
 }
 
 // See "Matching Paths" below to learn more
