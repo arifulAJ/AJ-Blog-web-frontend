fdddc6529544f9c7e47dd6963de73ef20ea9bddd
Ariful islam <arifulislam10111999@gmail.com>
2023-10-26 20:46:03 +0600

    nor

--- src/app/component/utils/privetRoute/privetRoute.jsx
+++ src/app/component/utils/privetRoute/privetRoute.jsx
@@ -1,41 +1,3 @@
-// "use client";
-// import { useRouter } from "next/navigation";
-// import React, { useEffect, useState } from "react";
-// import { getTokenFromServer } from "../tokenvarifay/tokenApi";
-// import Link from "next/link";
-// import LoginPage from "../../../login/page";
-
-// const withPrivateRoute = (WrappedComponent) => {
-//   return function PrivateRouteWrapper(props) {
-//     const [isToken, setIsToken] = useState(false);
-//     const router = useRouter();
-
-//     useEffect(() => {
-//       getTokenFromServer()
-//         .then((fetchedToken) => {
-//           if (fetchedToken) {
-//             setIsToken(true); // Token is present
-//           } else {
-//             router.push("/login"); // Token is not present, navigate to the login page
-//           }
-//         })
-//         .catch((error) => {
-//           console.error(error);
-//           // router.push("/login"); // Handle errors appropriately
-//         });
-//     }, [router]);
-
-//     if (isToken) {
-//       return <WrappedComponent {...props} />; // Render the protected content if the token is present
-//     } else {
-//       // You can also show a loading spinner or a message here while checking the token
-//       return router.push("/login");
-//     }
-//   };
-// };
-
-// export default withPrivateRoute;
-
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { getTokenFromServer } from "../tokenvarifay/tokenApi";
@@ -53,14 +15,12 @@ const withPrivateRoute = (WrappedComponent) => {
           if (fetchedToken) {
             setIsToken(true); // Token is present
           } else {
-            if (typeof window !== "undefined") {
-              router.push("/login"); // Client-side navigation
-            }
+            router.push("/login"); // Token is not present, navigate to the login page
           }
         })
         .catch((error) => {
           console.error(error);
-          // Handle errors appropriately
+          router.push("/login"); // Handle errors appropriately
         });
     }, [router]);
 
@@ -68,10 +28,7 @@ const withPrivateRoute = (WrappedComponent) => {
       return <WrappedComponent {...props} />; // Render the protected content if the token is present
     } else {
       // You can also show a loading spinner or a message here while checking the token
-      if (typeof window !== "undefined") {
-        router.push("/login"); // Client-side navigation
-      }
-      return null; // or any other placeholder component
+      return null;
     }
   };
 };
