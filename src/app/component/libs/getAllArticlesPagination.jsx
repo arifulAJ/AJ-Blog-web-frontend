75be613d0d3d7552496b5bd816a169bf928eed7b
Ariful islam <arifulislam10111999@gmail.com>
2023-10-28 11:33:03 +0600

    handel getway error

--- src/app/component/libs/getAllArticlesPagination.jsx
+++ src/app/component/libs/getAllArticlesPagination.jsx
@@ -1,8 +1,8 @@
-export default async function getAllArticlesPagination(page, limit) {
+export default async function getAllArticlesPagination() {
   // "http://localhost:8080/api/v1/auth/token"
   // "https://ar-blog-api.onrender.com/api/v1/articles?page=1&limit=10&sort_type=dec&sort_by=updatedAt"
   const res = await fetch(
-    `https://ar-blog-api.onrender.com/api/v1/articles?page=${page}&limit=${limit}&sort_type=dec&sort_by=updatedAt`,
+    `https://ar-blog-api.onrender.com/api/v1/articles?page=1&limit=10&sort_type=dec&sort_by=updatedAt`,
     { cache: "no-cache" }
   );
 
